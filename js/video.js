ff4caf8e4e0dc601965e84b71430983871ef0857
Mehrnaz Dorandish Yazdi <143748264+NazDoran@users.noreply.github.com>
2024-04-11 18:55:07 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,80 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
+	video.autopalay = false;
+	video.loop=false;
+	console.log ('auto play is set to '+ video.autoplay)
+	console.log ('loop is set to' + video.loop)
+	updateVolume();
+});
+function updateVolume() {
+    var volumeDisplay = document.querySelector("#volume");
+    volumeDisplay.textContent = video.volume * 100 + "%";
+}
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+    video.play();
+    updateVolume();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+    video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+    video.playbackRate /= 0.9;
+    console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+    if (video.currentTime + 10 > video.duration) {
+        video.currentTime = 0;
+    } else {
+        video.currentTime += 10;
+    }
+    console.log("Current location is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    video.muted = !video.muted;
+
+    if (video.muted) {
+        this.textContent = "Unmute";
+    } else {
+        this.textContent = "Mute";
+    }
+});
+
+var volumeSlider = document.querySelector("#slider");
+
+volumeSlider.addEventListener("input", function() {
+    var volume = volumeSlider.value / 100;
+    video.volume = volume;
+
+    updateVolume();
+});
+
+function updateVolume() {
+    var volumeDisplay = document.querySelector("#volume");
+    volumeDisplay.textContent = Math.round(video.volume * 100) + "%";
+}
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.toggle("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+});
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
