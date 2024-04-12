f1bc8bdf0196efe93dad3c5bffe9435b264e1bce
Mehrnaz Dorandish Yazdi <143748264+NazDoran@users.noreply.github.com>
2024-04-11 23:58:49 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,23 +1,29 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-	video = document.querySelector("#player1")
-	video.autopalay = false;
-	video.loop=false;
-	console.log ('auto play is set to '+ video.autoplay)
-	console.log ('loop is set to' + video.loop)
-	updateVolume();
+    console.log("Good job opening the window");
+    video = document.querySelector("#player1");
+    video.autoplay = false;
+    video.loop = false;
+
+    updateVolume();
+
+    console.log('auto play is set to ' + video.autoplay);
+    console.log('loop is set to ' + video.loop);
 });
+
 function updateVolume() {
     var volumeDisplay = document.querySelector("#volume");
-    volumeDisplay.textContent = video.volume * 100 + "%";
+    volumeDisplay.textContent = Math.round(video.volume * 100) + "%";
 }
+
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+    console.log("Play Video");
     video.play();
+
     updateVolume();
 });
+
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
@@ -74,7 +80,5 @@ document.querySelector("#vintage").addEventListener("click", function() {
 document.querySelector("#orig").addEventListener("click", function() {
     video.classList.remove("oldSchool");
 });
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+
 
